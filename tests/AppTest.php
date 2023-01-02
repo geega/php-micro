83d8d035be1fa95f5694259ea38e01881f79a97c
Svyatoslav Varpikhovsky <varpihovsky@gmail.com>
2023-01-02 14:55:37 +0200

    Init tests

---
+++ tests/AppTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace Geega\Micro\Tests;
+
+use Geega\Micro\App;
+use Geega\Micro\Http\Request;
+use Geega\Micro\Http\Response;
+use Geega\Micro\Router;
+use Geega\Micro\Tests\AppTest\Controller\MainController;
+use PHPUnit\Framework\TestCase;
+
+
+class AppTest extends TestCase
+{
+    public function testRun()
+    {
+        $this->markTestSkipped('Need refactoring');
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['REQUEST_URI'] = '/';
+        $request = new Request();
+        $router = new Router($request, '\\Geega\\Micro\\Tests\\AppTest\\Controller\\{{NAME}}Controller');
+        $router->get('/', 'Main@actionTest');
+        $app = new App($router, '');
+
+        ob_start();
+        $app->run();
+        $content = ob_get_clean();
+    }
+
+    public function testCreateResponse()
+    {
+        $request = new Request();
+        $router = new Router($request, '\\Geega\\Micro\\Tests\\AppTest\\Controller\\{{NAME}}Controller');
+        $app = new App($router, '');
+
+        $response = $app->createResponse();
+
+        $this->assertInstanceOf(Response::class, $response);
+    }
+}
---
+++ tests/AppTest/Controller/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Geega\Micro\Tests\AppTest\Controller;
+
+use Geega\Micro\Controller;
+
+class MainController extends Controller
+{
+    public function actionTest()
+    {
+        return 'test';
+    }
+}
